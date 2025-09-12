e82f7a1ea168a1c2af55ac06b418c702bc9b9f54
Luka Jorin Dittmann <187892468+luckyluck008@users.noreply.github.com>
2025-09-12 12:18:17 +0200

    Add Primzahl class to check for prime numbers

---
+++ Primzahl.java
@@ -0,0 +1,27 @@
+public class Primzahl {
+    private int Zahlen;  // Speichert, wie viele Zahlen geprüft werden sollen
+
+    public Primzahl(int Zahlen) {
+        this.Zahlen = Zahlen;
+    }
+
+    public void Rechnung() {
+        for (int i = 2; i <= Zahlen; i++) {  // Prüfe Zahlen von 2 bis Zahlen
+            if (istPrimzahl(i)) {
+                System.out.println(i + " ist eine Primzahl.");
+            }
+        }
+    }
+
+    private boolean istPrimzahl(int zahl) {
+        if (zahl < 2) return false;
+
+        for (int teiler = 2; teiler <= zahl / 2; teiler++) {
+            if (zahl % teiler == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
