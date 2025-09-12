325d490eee0beff6a2e914b4b1b5b192f43d62c4
Luka Jorin Dittmann <187892468+luckyluck008@users.noreply.github.com>
2025-09-12 11:45:32 +0200

    Refactor Bruch class, burch does work now, euclidic algorithm removed

--- Bruch.java
+++ Bruch.java
@@ -2,10 +2,6 @@ public class Bruch
 {
     private int zaehler;
     private int nenner;
-    private int zweiZaehler;
-    private int zweiNenner;
-    private int result;
-
     public Bruch(int zaehler, int nenner)
     {
         this.zaehler = zaehler;
@@ -54,37 +50,9 @@ public class Bruch
         return (1.0 * this.zaehler) / this.nenner;
     }
 
-    public int euklidischerAlgorithmus()
+    public void dezimalAusgeben() 
     {
-        int a = Math.abs(this.zaehler);
-        int b = Math.abs(this.nenner);
+        System.out.println("Dezimalwert:" + this.dezimalzahlBerechnen());
 
-        if (b > a)
-        {
-            int hilf = a;
-            a = b;
-            b = hilf;
-        }
-
-        while (b != 0)
-        {
-            int rest = a % b;
-            a = b;
-            b = rest;
-        }
-
-        return a;
-    }
-    public int bruchAddieren(Bruch ersterBruch, Bruch zweiterBruch)
-    {
-        int a = Math.abs(this.ersterZaehler);
-        int b = Math.abs(this.ersterNenner);
-        int c = Math.abs(this.zweiZaehler);
-        int d = Math.abs(this.zweiNenner);
-        
-        
-       
-    
-        return this.result;
     }
 }
